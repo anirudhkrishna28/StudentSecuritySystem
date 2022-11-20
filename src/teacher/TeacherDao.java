7aded3615e3b00dd08d7c20b3f91e0b77865d23a
anirudhkrishna28 <116937743+anirudhkrishna28@users.noreply.github.com>
2022-11-21 02:10:17 +1100

    Update TeacherDao.java

--- src/teacher/TeacherDao.java
+++ src/teacher/TeacherDao.java
@@ -1,20 +1,45 @@
 package teacher;
 
 public class TeacherDao {
-    //database connection
-
-    public void writeEvent()
-    {
-        //stores the event participated by a student in database
+    
+    public static void writeAttendance(Connection con,Teacher t,Integer id) throws SQLException {
+        //writes attendance into database
+        String query = "update studentsecurity set attendance = '"+t.getAttendance()+"' where rollno = "+id;
+//        System.out.println(query);
+        PreparedStatement st  = con.prepareStatement(query);
+        st.executeUpdate();
     }
 
-    public void writePresent()
+    public static  void writeEvent(Connection con,Teacher t,Integer id) throws SQLException
     {
-        //stores present or absent for the student
+        //writes event into database
+        String query = "update studentsecurity set event = '"+t.getEvent()+"' where rollno = "+id;
+//        System.out.println(query);
+        PreparedStatement st  = con.prepareStatement(query);
+        st.executeUpdate();
+
+
     }
 
+    public static void main(String[] args) throws Exception{
+        String url="jdbc:mysql://localhost:3306/test";
+        String uname="root";
+        String pass="Goniya030612#";
+        String query="select * from studentsecurity";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url,uname,pass);
+        Teacher t = new Teacher();
+        t.setAttendance("P");
+        t.setEvent("PaperPresentation");
+        writeAttendance(con,t,13);
+        writeEvent(con,t,13);
 
-
+    }
 }
+
+
+
+
+
 
 
