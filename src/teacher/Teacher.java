53c3813c4f5d443ef86b6895985bdaf15c073dee
anirudhkrishna28 <anirudhkrishna.s2021csea@sece.ac.in>
2022-11-20 23:03:15 +0530

    hi

--- src/teacher/Teacher.java
+++ src/teacher/Teacher.java
@@ -1,9 +1,48 @@
 package teacher;
- public class Teacher {
+
+import watchman.WatchMan;
+
+public class Teacher{
 	 private String event;
 	   private String attendance;
 
+	public String getIntime() {
+		return intime;
+	}
+
+	public void setIntime(String intime) {
+		this.intime = intime;
+	}
+
+	public String getOuttime() {
+		return outtime;
+	}
+
+	public void setOuttime(String outtime) {
+		this.outtime = outtime;
+	}
+
+	private String intime,outtime;
 	    private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
 
 	    public String getEvent() {
 	        return event;
--- src/teacher/TeacherDao.java
+++ src/teacher/TeacherDao.java
@@ -1,12 +1,17 @@
 package teacher;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import student.Student;
 
-public class TeacherDao {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TeacherDao extends Student {
+
+    static int noOfStud = 8;
+    static List<Teacher> stud = new ArrayList<Teacher>();
+
+    static List<Teacher> studEvent  = new ArrayList<Teacher>();
     
     public static void writeAttendance(Connection con, Teacher t, Integer id) throws SQLException {
         //writes attendance into database
@@ -27,18 +32,55 @@ public class TeacherDao {
 
     }
 
+    public static void displayStudnetsPresent(Connection con) throws SQLException {
+        Statement st  = con.createStatement();
+        String query = "select s.Name_of_Student,w.In_Time,w.Out_Time from Student s,Watchman w where s.Bus_Number=w.Bus_no and Present_Absent='P'";
+        ResultSet rs = st.executeQuery(query);
+
+
+
+
+        while(rs.next()) {
+            Teacher t = new Teacher();
+            String name = rs.getString("Name_of_Student");
+            String inTime = rs.getString("In_Time");
+            String outTime = rs.getString("Out_Time");
+            t.setName(name);
+            t.setAttendance("P");
+            t.setIntime(inTime);
+            t.setOuttime(outTime);
+
+            stud.add(t);
+
+            //System.out.println("name :"+name+"  Intime "+inTime+"  outtime :"+outTime);
+        }
+
+    }
+
+    public static void display()
+    {
+        int i= 3;
+        while(i-->0)
+        {
+            System.out.println("name: "+stud.get(i).getName()+" "+"  Intime: "+stud.get(i).getIntime()+"  OutTime: "+stud.get(i).getOuttime());
+        }
+
+    }
+
     public static void main(String[] args) throws Exception{
         String url="jdbc:mysql://localhost:3306/ani";
         String uname="root";
         String pass="anirudh";
-        String query="select * from studentsecurity";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url,uname,pass);
-        Teacher t = new Teacher();
-        t.setAttendance("P");
-        t.setEvent("PaperPresentation");
-        writeAttendance(con,t,13);
-        writeEvent(con,t,13);
+
+        displayStudnetsPresent(con);
+        display();
+//        Teacher t = new Teacher();
+//        t.setAttendance("P");
+//        t.setEvent("PaperPresentation");
+//        writeAttendance(con,t,13);
+//        writeEvent(con,t,13);
 
     }
 }
